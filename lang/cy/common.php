73c2c59a7ed12e2e2b284fe9be6b87ba45702755
raphaelcastrocriap <raphaelcastro@criap.com>
2024-09-24 14:37:01 +0100

    v1

---
+++ lang/cy/common.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Common elements found throughout many areas of BookStack.
+ */
+return [
+
+    // Buttons
+    'cancel' => 'Canslo',
+    'close' => 'Cau',
+    'confirm' => 'Cadarnhau',
+    'back' => 'Yn ôl',
+    'save' => 'Cadw',
+    'continue' => 'Parhau',
+    'select' => 'Dewis',
+    'toggle_all' => 'Toglo Popeth',
+    'more' => 'Mwy',
+
+    // Form Labels
+    'name' => 'Enw',
+    'description' => 'Disgrifiad',
+    'role' => 'Rôl',
+    'cover_image' => 'Cover image',
+    'cover_image_description' => 'This image should be approximately 440x250px although it will be flexibly scaled & cropped to fit the user interface in different scenarios as required, so actual dimensions for display will differ.',
+
+    // Actions
+    'actions' => 'Gweithredoedd',
+    'view' => 'Gweld',
+    'view_all' => 'Gweld popeth',
+    'new' => 'Newydd',
+    'create' => 'Creu',
+    'update' => 'Diweddaru',
+    'edit' => 'Golygu',
+    'sort' => 'Trefnu',
+    'move' => 'Symud',
+    'copy' => 'Copïo',
+    'reply' => 'Ateb',
+    'delete' => 'Dileu',
+    'delete_confirm' => 'Cadarnhau\'r dilead',
+    'search' => 'Chwilio',
+    'search_clear' => 'Clirio\'r chwiliad',
+    'reset' => 'Ailosod',
+    'remove' => 'Diddymu',
+    'add' => 'Ychwanegu',
+    'configure' => 'Ffurfweddu',
+    'manage' => 'Rheoli',
+    'fullscreen' => 'Sgrin Llawn',
+    'favourite' => 'Gwneud Ffefryn',
+    'unfavourite' => 'Dadwneud Ffefryn',
+    'next' => 'Nesa',
+    'previous' => 'Cynt',
+    'filter_active' => 'Hidl weithredol:',
+    'filter_clear' => 'Clirio\'r hidl',
+    'download' => 'Llwytho i lawr',
+    'open_in_tab' => 'Agor mewn Tab',
+    'open' => 'Agor',
+
+    // Sort Options
+    'sort_options' => 'Trefnu\'r opsiynau',
+    'sort_direction_toggle' => 'Sort Direction Toggle',
+    'sort_ascending' => 'Trefnu\'n esgynnol',
+    'sort_descending' => 'Trefnu\'n ddisgynnol',
+    'sort_name' => 'Enw',
+    'sort_default' => 'Diofyn',
+    'sort_created_at' => 'Dyddiad Creu',
+    'sort_updated_at' => 'Dyddiad Diweddaru',
+
+    // Misc
+    'deleted_user' => 'Defnyddiwr wedi\'i Dileu',
+    'no_activity' => 'Dim actifedd i arddangos',
+    'no_items' => 'Dim eitemau ar gael',
+    'back_to_top' => 'Yn ôl i\'r brig',
+    'skip_to_main_content' => 'Neidio i\'r prif gynnwys',
+    'toggle_details' => 'Toglo Manylion',
+    'toggle_thumbnails' => 'Toglo Mân-Luniau',
+    'details' => 'Manylion',
+    'grid_view' => 'Golwg Grid',
+    'list_view' => 'Golwg Rhestr',
+    'default' => 'Diofyn',
+    'breadcrumb' => 'Breadcrumb',
+    'status' => 'Statws',
+    'status_active' => 'Gweithredol',
+    'status_inactive' => 'Anweithredol',
+    'never' => 'Byth',
+    'none' => 'Dim un',
+
+    // Header
+    'homepage' => 'Tudalen cartref',
+    'header_menu_expand' => 'Expand Header Menu',
+    'profile_menu' => 'Dewislen Proffil',
+    'view_profile' => 'Gweld proffil',
+    'edit_profile' => 'Golygu Proffil',
+    'dark_mode' => 'Modd Tywyll',
+    'light_mode' => 'Modd Golau',
+    'global_search' => 'Global Search',
+
+    // Layout tabs
+    'tab_info' => 'Gwybodaeth',
+    'tab_info_label' => 'Tab: Show Secondary Information',
+    'tab_content' => 'Cynnwys',
+    'tab_content_label' => 'Tab: Show Primary Content',
+
+    // Email Content
+    'email_action_help' => 'If you’re having trouble clicking the ":actionText" button, copy and paste the URL below into your web browser:',
+    'email_rights' => 'All rights reserved',
+
+    // Footer Link Options
+    // Not directly used but available for convenience to users.
+    'privacy_policy' => 'Polisi Preifatrwydd',
+    'terms_of_service' => 'Terms of Service',
+];
